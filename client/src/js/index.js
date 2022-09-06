be01b28893a65aa4ae428ce97643eba4d3b47e87
Marco Mata <patrickstar1281@yahoo.com>
2022-09-06 01:47:39 -0500

    set up indexeddb

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -3,19 +3,18 @@ import "./submit";
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
-
 //import modules
 import "./form";
 import "./submit";
-
 //import css files
 import "../css/index.css";
-
 //css & bootstrap stuff
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { initdb } from './database';
 
 window.addEventListener('load', function () {
+    initdb();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
